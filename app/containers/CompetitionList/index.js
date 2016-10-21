1336f8e91aed9d09715131e526f5fb626e93752b
Ilya Timofeev <ilya.timofeev@ru.axxonsoft.com>
2016-10-22 01:14:38 +0300

    Список соревнований со ссылками

--- app/containers/CompetitionList/index.js
+++ app/containers/CompetitionList/index.js
@@ -7,10 +7,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
-import selectCompetitionListProp from './selectors';
+import { selectCompetitionListProp } from './selectors';
 import styles from './styles.css';
 import { createStructuredSelector } from 'reselect';
 import { loadCompetitionList } from './actions';
+import { push } from 'react-router-redux';
+
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
+
 
 export class CompetitionList extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
@@ -21,14 +25,35 @@ export class CompetitionList extends React.Component { // eslint-disable-line re
     this.props.loadCompetitionList();
   }
 
+  openCompetitionPage = (id) => {
+    this.props.changeRoute(`/competitions/${id}`);
+  };
+
   render() {
     const competitionList = this.props.competitionList;
 
     let listRender;
 
     if (competitionList) {
-      listRender = (<div>hello, there</div>
-
+      listRender = (
+        <Table selectable={false}>
+          <TableHeader displaySelectAll={false}>
+            <TableRow>
+              <TableHeaderColumn>Date</TableHeaderColumn>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox={false}>
+            {competitionList.content.map((item, index) =>
+              <TableRow key={index}>
+                <TableRowColumn>{item.Date}</TableRowColumn>
+                <TableRowColumn>
+                  <a onClick={() => this.openCompetitionPage(item.id)}>{item.title}</a>
+                </TableRowColumn>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
       );
     }
 
@@ -52,6 +77,7 @@ CompetitionList.propTypes = {
     React.PropTypes.bool,
   ]),
   loadCompetitionList: React.PropTypes.func,
+  changeRoute: React.PropTypes.func,
 };
 
 const mapStateToProps = createStructuredSelector({
@@ -62,6 +88,7 @@ function mapDispatchToProps(dispatch) {
   return {
     dispatch,
     loadCompetitionList: () => dispatch(loadCompetitionList()),
+    changeRoute: (url) => dispatch(push(url)),
   };
 }
 
